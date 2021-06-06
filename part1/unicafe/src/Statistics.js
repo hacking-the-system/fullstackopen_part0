2ca1bdc23f70f37637a0777d07be2e93c53d6421
Josep Pascual <joseppascualbadia@gmail.com>
2021-06-06 21:54:58 +0200

    exercise 1.7

--- part1/unicafe/src/Statistics.js
+++ part1/unicafe/src/Statistics.js
@@ -8,10 +8,39 @@ const statistics = ({ values }) => {
       </p>
     );
   });
+  const all = values.map((element) => element.value).reduce((a, b) => a + b);
+  const average =
+    values
+      .map((element) => {
+        if (element.label === "good") {
+          return element.value;
+        } else if (element.label === "neutral") {
+          return 0;
+        } else {
+          // (element.label === "bad")
+          return element.value * -1;
+        }
+      })
+      .reduce((a, b) => a + b) / all;
+  const positive =
+    (values
+      .map((element) => {
+        if (element.label === "good") {
+          return element.value;
+        } else {
+          return 0;
+        }
+      })
+      .reduce((a, b) => a + b) /
+      all) *
+    100;
   return (
     <div>
       <Title text="statistics" />
       {displayValues}
+      <p>all {all}</p>
+      <p>average {isNaN(average) ? 0 : average}</p>
+      <p>positive {isNaN(positive) ? 0 : positive}%</p>
     </div>
   );
 };
