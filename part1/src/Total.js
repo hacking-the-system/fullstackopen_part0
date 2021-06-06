39e5c3f633ab814ac2045c6c9f4e848623baf710
Josep Pascual <joseppascualbadia@gmail.com>
2021-06-06 20:11:37 +0200

    Fix formatting

--- part1/src/Total.js
+++ part1/src/Total.js
@@ -1,6 +1,7 @@
 const Total = (props) => {
-  const total = props.parts.map(part => part.exercises)
-  .reduce((a, b) => a + b);
+  const total = props.parts
+    .map((part) => part.exercises)
+    .reduce((a, b) => a + b);
   return <p>Number of exercises {total}</p>;
 };
 
